109e8d85a7024353d535183d1256fc68859de9b1
entrepreneur123 <46771494+entrepreneur123@users.noreply.github.com>
2020-06-25 22:58:36 +0545

    Update config.php

--- app/config.php
+++ app/config.php
@@ -1,9 +1,9 @@
 <?php
 
  return $config = [
-    'account_sid'=>'AC5cd61e68b2f7cb83723bbdf217215cbf',
-    'auth_token' =>'1961ce407625046f509e8fb1feecb699',
-    'phone_number'=>'(336) 490-2873'
+    'account_sid'=>'#',
+    'auth_token' =>'#',
+    'phone_number'=>'#'
 
 
 
